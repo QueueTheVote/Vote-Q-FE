d558236800613b10fa44f2022b058e5c7f69bb98
Helen Liutongco <liutongcoh@gmail.com>
2020-06-14 13:16:43 -0400

    add detail page code

--- src/components/queue/QueueContainer.js
+++ src/components/queue/QueueContainer.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function QueueContainer() {
   return (
-    <div className="card">
-      Placeholder for QueueContainer
-    </div>
+    <>
+      <div className="card-body">
+        Placeholder for Checklist
+      </div>
+      <div className="card-footer">
+        Placeholder for QueueForm
+      </div>
+    </>
   )
 }
 
